fbc756c862b40410da9788d0dd6825e0c060dd49
5072-Ramco <ramu.devaraj@gmail.com>
2025-01-01 15:47:36 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,14 @@
+function skillsMember() {
+    return {
+        name: 'John Doe',
+        age: 25,
+        skills: ['HTML', 'CSS', 'JS'],
+        // Method
+        showSkills: function() {
+            this.skills.forEach(function(skill) {
+                console.log(skill);
+            });
+        }
+    };
+}
+
